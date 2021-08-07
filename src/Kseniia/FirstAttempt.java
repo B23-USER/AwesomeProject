6789d79e5e55d85e9b5b7a5bb713f84f25e1b203
KseniiaMazanko <ksmazanko@gmail.com>
2021-08-06 21:41:04 -0400

    made the second commit

--- src/Kseniia/FirstAttempt.java
+++ src/Kseniia/FirstAttempt.java
@@ -5,7 +5,7 @@ public class FirstAttempt {
     public static void main(String[] args) {
 
         System.out.println("First attempt to fork and make changes to someone else's file");
-
+        //commit 2
     }
 
 }
